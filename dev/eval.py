db37172b5d33777a0be8232e5543c4c935e41630
msaad02 <msaad02@outlook.com>
2023-04-17 11:03:44 -0400

    update organization

--- dev/eval.py
+++ dev/eval.py
@@ -61,3 +61,44 @@ def vectorize(fen):
     
 print(vectorize(r'rnbqkb1r/ppp1pppp/3p4/3nP3/3P4/8/PPPK1PPP/RNBQ1BNR b kq - 1 4')[0])
 print(vectorize(r'r3r1k1/pppq2bp/3pp1p1/3P4/2PpP3/6P1/PP1N2K1/R1BQR3 b - e3 0 19')[0])
+
+import numpy as np
+import tensorflow as tf
+
+def train_neural_network(bit_vector):
+    # Define the neural network architecture
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(13, 8, 8)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    # Compile the model
+    model.compile(optimizer='rmsprop',
+                  loss='mean_squared_error',
+                  metrics=['accuracy'])
+
+    # Prepare the training data
+    x_train = np.array([bit_vector])
+    y_train = np.array([0])  # Replace 0 with the actual label of the position
+
+    # Train the model
+    model.fit(x_train, y_train, epochs=10, batch_size=1)
+    x_test = np.array([bit_vector])
+    y_test = np.array([0])  # Replace 0 with the actual label of the test position
+
+    # Evaluate the model on the test data
+    loss, accuracy = model.evaluate(x_test, y_test)
+
+    # Print the evaluation metrics
+    print(f"Test loss: {loss:.4f}")
+    print(f"Test accuracy: {accuracy:.4f}")
+
+
+import pandas as pd
+
+names = ['PuzzleId','FEN','Moves','Rating','RatingDeviation','Popularity','NbPlays','Themes','GameUrl','OpeningTags']
+df = pd.read_csv('../data/kaggleDataset/chessData.csv', names = names)
+
+train_neural_network(vectorize(df.FEN))
