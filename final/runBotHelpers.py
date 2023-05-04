182f20252b4a53878be591e73739426221e60459
Austin Barr <abarr2118@gmail.com>
2023-05-04 13:49:01 -0400

    added checkmate handling to minimax

--- final/runBotHelpers.py
+++ final/runBotHelpers.py
@@ -36,15 +36,19 @@ def find_best_move(board, depth, maximizing_player, model):
 #         return evaluate(board, model)
 
 def minimax(board, depth, alpha, beta, maximizing_player, model):
-    if board.is_game_over() or depth == 0:
-        return evaluate(board, model)
+    if board.is_checkmate():
+        return -10000 if maximizing_player else 10000
+    if board.is_game_over(claim_draw=True):
+        return 9000 if maximizing_player else -9000
+    if depth == 0:
+        return evaluate(board)
     
     if maximizing_player:
         best_score = float('-inf')
         for move in board.legal_moves:
             board.push(move)
             score = minimax(board, depth-1, alpha, beta, False, model)
-            #score += depth
+            score += depth
             board.pop()
             best_score = max(score, best_score)
             alpha = max(alpha, best_score)
@@ -56,7 +60,7 @@ def minimax(board, depth, alpha, beta, maximizing_player, model):
         for move in board.legal_moves:
             board.push(move)
             score = minimax(board, depth-1, alpha, beta, True, model)
-            #score -= depth
+            score -= depth
             board.pop()
             best_score = min(score, best_score)
             beta = min(beta, best_score)
